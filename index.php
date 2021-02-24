c2d3178ba2935ddeffcb4f6a33ae64ba918554a5
ArianNexux <ariannexux0101@gmail.com>
2021-02-24 09:42:15 +0100

    First Commit

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-// Include the qrlib file 
+
 include 'phpqrcode/qrlib.php'; 
-$text = uniqid(); 
-  
-// $path variable store the location where to  
-// store image and $file creates directory name 
-// of the QR code file by using 'uniqid' 
-// uniqid creates unique id based on microtime
+$text = "Arian D."; 
+
 $path = 'images'; 
 
 if(!is_dir($path)) 
@@ -14,12 +10,10 @@ if(!is_dir($path))
 
 $file = $path."/".uniqid().".png"; 
   
-// $ecc stores error correction capability('L') 
 $ecc = 'L'; 
 $pixel_size = 10; 
 $frame_size = 10; 
   
-// Generates QR Code and Stores it in directory given 
 QRcode::png($text, $file, $ecc, $pixel_size, $frame_size); 
   
 ?>
